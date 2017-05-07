b6351e6bf314e0f94f62d66cc6cb0c8dcb8bc4ba
KwatME <kwat.medetgul.ernar@gmail.com>
2017-05-07 15:43:50 -0700

    Rap

--- mutational_signature/mutational_signature.py
+++ mutational_signature/mutational_signature.py
@@ -1,16 +1,3 @@
-"""
-Computational Cancer Analysis Library
-
-Authors:
-    Huwate (Kwat) Yeerna (Medetgul-Ernar)
-        kwat.medetgul.ernar@gmail.com
-        Computational Cancer Analysis Laboratory, UCSD Moore's Cancer Center
-
-    Pablo Tamayo
-        ptamayo@ucsd.edu
-        Computational Cancer Analysis Laboratory, UCSD Moore's Cancer Center
-"""
-
 import copy
 import re
 from pprint import pprint
